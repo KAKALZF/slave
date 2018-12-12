75acda08c589a49e7962227300db0cca6b2e5dfb
KAKALZF <575800503@qq.com>
2018-12-12 17:18:50 +0800

    通过JPA练习用户，角色，资源

--- src/main/java/com/example/slave/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.slave.dao;
-
-import com.example.slave.entity.User;
-
-public interface UserRepository extends BaseRepository<User,Long>{
-    User findByName(String name);
-}
-
--- src/main/java/com/example/slave/jpa/dao/Employee2Dao.java
+++
@@ -1,9 +0,0 @@
-package com.example.slave.jpa.dao;
-
-import com.example.slave.jpa._02_many2one.Employee2;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Employee2Dao extends JpaRepository<Employee2, Long> {
-}
